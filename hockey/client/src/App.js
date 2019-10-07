51f26f2f0b11fc8f5c5ce2e4b888ae4efba20580
Curtis Jochen <curtis.jochen@gmail.com>
2019-10-07 15:52:51 -0400

    Changed file lay out slightly

--- hockey/client/src/App.js
+++ hockey/client/src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import LoginPage from "./components/LoginPage";
-import Title from "./components/Title";
+import LoginPage from "./pages/LoginPage";
+
 
 
 function App () {
     return (
       <Router>
         <div>
-          <Title />
           <LoginPage />
         </div>
       </Router>
